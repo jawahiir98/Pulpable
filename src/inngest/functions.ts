8d64f83d944743df6e7c6cf2ddcfeab2e390d76d
Jawahiiir <jawahiirn@gmail.com>
2025-08-06 22:18:20 +0600

    E2B Sandbox template created. Docker image running.

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
 import { inngest } from './client';
 import { openai, createAgent } from '@inngest/agent-kit';
+import { Sandbox } from '@e2b/code-interpreter';
+import { getSandbox } from '@/inngest/utils';
 
 export const helloWorld = inngest.createFunction(
   { id: 'hello-world' },
   { event: 'test/hello.world' },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run('get-sandbox-id', async () => {
+      const sandbox = await Sandbox.create('pulpable-nextjs-dev');
+      return sandbox.sandboxId;
+    });
+
     const codeAgent = createAgent({
       name: 'code-agent',
       system:
@@ -16,7 +23,11 @@ export const helloWorld = inngest.createFunction(
       `Write the following snippet: ${event.data.value}`
     );
 
-    console.log(' summary ', output);
-    return { output };
+    const sandboxUrl = await step.run('get-sanbox-url', async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+    return { output, sandboxUrl };
   }
 );
