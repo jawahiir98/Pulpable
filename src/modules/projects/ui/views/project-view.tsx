c025f8c861a9a068f3f68cf0d22fdc846383371f
Jawahiiir <jawahiirn@gmail.com>
2025-08-09 15:11:59 +0600

    Resizable UI for project and messages UI.

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -2,6 +2,11 @@
 
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { useTRPC } from '@/trpc/client';
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable';
 
 interface Props {
   projectId: string;
@@ -21,8 +26,23 @@ export const ProjectView = ({ projectId }: Props) => {
   );
   return (
     <div>
-      <div className="">{JSON.stringify(project)}</div>
-      <div className="">{JSON.stringify(messages)}</div>
+      <ResizablePanelGroup direction={'horizontal'}>
+        <ResizablePanel
+          defaultSize={35}
+          minSize={20}
+          className={'flex flex-col min-h-0'}
+        >
+          {JSON.stringify(project)}
+        </ResizablePanel>
+        <ResizableHandle withHandle={true} />
+        <ResizablePanel
+          defaultSize={65}
+          minSize={50}
+          className={'flex flex-col min-h-0'}
+        >
+          {JSON.stringify(messages)}
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 };
