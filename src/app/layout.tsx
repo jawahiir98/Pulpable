ec3e45f43db9133c79d415502e1d344f5fedae6a
Jawahiiir <jawahiirn@gmail.com>
2025-08-16 00:35:02 +0600

    project header.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { TRPCReactProvider } from '@/trpc/client';
+import { ThemeProvider } from 'next-themes';
+import { Toaster } from 'sonner';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -24,11 +26,18 @@ export default function RootLayout({
 }>) {
   return (
     <TRPCReactProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          {children}
+          <ThemeProvider
+            attribute={'class'}
+            defaultTheme={'system'}
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Toaster /> {children}
+          </ThemeProvider>
         </body>
       </html>
     </TRPCReactProvider>
