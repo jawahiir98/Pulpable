b44adc4c7bed97d1f7475f2b05627f4d8e2a55d1
Jawahiiir <jawahiirn@gmail.com>
2025-09-01 23:02:49 +0600

    zod and functions changed

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -246,7 +246,7 @@ export const codeAgentFunction = inngest.createFunction(
 
     await step.run('save-result', async () => {
       if (isError) {
-        return await primsa.message.create({
+        return await prisma.message.create({
           data: {
             projectId: event.data.projectId,
             content: 'Something went wrong. Please try again.',
